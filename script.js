54e5f075190d4246c4dee4dcb048b3654bf3201c
Narayan Aryal <aryal.coding@gmail.com>
2022-10-15 14:54:25 +1100

    done
    git commit -m done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 let taskList = []
 
-const badList = []
+let badList = []
 
 const hourPerWeek = 24 * 7;
 
@@ -15,7 +15,7 @@ const handleOnSubmit = (e) =>{
     
 const obj = {task, hr};
 
-const totalAllocatedHours = totalTaskHours();
+const totalAllocatedHours = totalTaskHours() + totalBadTaskHours();
 
 if(totalAllocatedHours + hr > hourPerWeek){
     return alert("Sorry, you exceeds the weekly time");
@@ -34,19 +34,44 @@ const displayTasks = () => {
   taskList.map((item, i) => {
     console.log(item, i);
     str += `<tr>
-    <th scope="row">1</th>
+    <th scope="row">${i+1}</th>
     <td>${item.task}</td>
     <td>${item.hr}</td>
     <td class="text-end">
         <button onclick = "deleteTask(${i})" class="btn btn-danger"><i class="fa-solid fa-trash"></i> </button>
-        <button class="btn btn-success"><i class="fa-solid fa-right-long"></i> </button>
+        <button onclick = " markAsNotToDo(${i})" class="btn btn-success"><i class="fa-solid fa-right-long"></i> </button>
     </td>
   </tr>`;
-
   });
 
 document.getElementById('task-list').innerHTML= str;
 }
+
+
+const displayBadTask= ()=>{
+    let str = ""
+    badList.map((item, i) => {
+        str += `<tr>
+        <th scope="row">${i + 1}</th>
+        <td> ${item.task}</td>
+        <td> ${item.hr}</td>
+        <td class="text-end">
+            <button onclick = "markAsToDo(${i})" class="btn btn-warning"><i class="fa-solid fa-left-long"></i> </button>
+            <button onclick = "deleteBadTask(${i})" class="btn btn-danger"><i class="fa-solid fa-trash"></i> </button>
+         
+
+        </td>
+      </tr>`
+    });
+
+    document.getElementById('bad-task').innerHTML = str;
+
+    totalBadTaskHours();
+};
+
+
+
+
 
 const totalTaskHours = () => {
     const total = taskList.reduce((subTtl, item) => {
@@ -54,7 +79,18 @@ const totalTaskHours = () => {
     }, 0);
 console.log(total);
 
-document.getElementById('totalHours').innerText = total;
+document.getElementById('totalHours').innerText = total + totalBadTaskHours() ;
+
+return total;
+};
+
+const totalBadTaskHours = () => {
+    const total = badList.reduce((subTtl, item) => {
+        return subTtl + item.hr;
+    }, 0);
+console.log(total);
+
+document.getElementById('totalBadHr').innerText = total;
 
 return total;
 };
@@ -65,8 +101,36 @@ const deleteTask = (i) => {
     }
     taskList = taskList.filter((item, index)=> index !== i);
             
-    
 
     displayTasks();
     totalTaskHours();
-;}
+};
+
+const deleteBadTask = (i) => {
+    if(!window.confirm("Are you sure you want to delete this task?")){
+        return;
+    }
+    badList = badList.filter((item, index)=> index !== i);
+            
+
+    displayBadTask();
+    totalTaskHours();
+    
+};
+
+const markAsNotToDo = (i) => {
+    const itm = taskList.splice(i, 1);
+    badList.push(itm[0]);
+    
+    displayTasks();
+    displayBadTask();
+    
+};
+const markAsToDo = (i) => {
+    const a = badList.splice(i, 1);
+    taskList.push(a[0]);
+    
+    displayTasks();
+    displayBadTask();
+    
+};
